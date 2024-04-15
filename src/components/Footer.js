87bcef912dbfd366ac66e0c47ce43485818bd652
Kirill Chubuk <kirillchubuk@cloud.upwork.com>
2024-04-15 18:06:49 +0200

    Create basic components

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import Nav from "./Nav";
+
+export default function Footer() {
+  return (
+    <footer>
+      <h4>Navigation</h4>
+      <Nav></Nav>
+      <h4>Contact</h4>
+      <h4>Social Media Links</h4>
+    </footer>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import Nav from "./Nav";
+
+export default function Header() {
+  return (
+    <header>
+      <img src="assets/Logo.jpg" width="202" height="76"></img>
+      <Nav></Nav>
+    </header>
+  );
+}
